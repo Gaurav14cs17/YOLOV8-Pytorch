d6ab7caa7a9c7404c18fcce94c6234335e48ee43
Gaurav <33745822+Gaurav14cs17@users.noreply.github.com>
2025-08-09 09:34:49 +0530

    Update backbone.py

--- model/backbone.py
+++ model/backbone.py
@@ -1,27 +1,27 @@
+# backbone.py
 import torch
-import torch.nn as nn
-from .blocks import Conv, CSP, SPP
+from blocks import ConvBlock, CSPBlock, SPPBlock
 
-class DarkNet(nn.Module):
+class Backbone(torch.nn.Module):
     def __init__(self, width, depth):
         super().__init__()
-        self.stage1 = Conv(width[0], width[1], 3, 2)
-        self.stage2 = nn.Sequential(
-            Conv(width[1], width[2], 3, 2),
-            CSP(width[2], width[2], depth[0])
+        self.stage1 = torch.nn.Sequential(ConvBlock(width[0], width[1], 3, 2))
+        self.stage2 = torch.nn.Sequential(
+            ConvBlock(width[1], width[2], 3, 2),
+            CSPBlock(width[2], width[2], depth[0])
         )
-        self.stage3 = nn.Sequential(
-            Conv(width[2], width[3], 3, 2),
-            CSP(width[3], width[3], depth[1])
+        self.stage3 = torch.nn.Sequential(
+            ConvBlock(width[2], width[3], 3, 2),
+            CSPBlock(width[3], width[3], depth[1])
         )
-        self.stage4 = nn.Sequential(
-            Conv(width[3], width[4], 3, 2),
-            CSP(width[4], width[4], depth[2])
+        self.stage4 = torch.nn.Sequential(
+            ConvBlock(width[3], width[4], 3, 2),
+            CSPBlock(width[4], width[4], depth[2])
         )
-        self.stage5 = nn.Sequential(
-            Conv(width[4], width[5], 3, 2),
-            CSP(width[5], width[5], depth[0]),
-            SPP(width[5], width[5])
+        self.stage5 = torch.nn.Sequential(
+            ConvBlock(width[4], width[5], 3, 2),
+            CSPBlock(width[5], width[5], depth[0]),
+            SPPBlock(width[5], width[5])
         )
 
     def forward(self, x):
